f775e7f431c7ea28db422463c03d0743102a2673
Joseph Martinez <insertepala@gmail.com>
2024-06-25 11:10:00 -0500

    Latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = () => {
+    setCount((prev) => prev + 1)
+  }
+
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <p>{count}</p>
+      <button onClick={handleClick}>Click me!</button>
+    </div>
   )
 }
 
